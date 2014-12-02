e5392966267aebb9cb28df1e1c1849da5e2ed9f0
Todd Hale <tdhale@xactware.com>
2014-12-02 16:50:44 -0700

    part 6

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 var app = {
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -15,17 +8,10 @@ var app = {
         }
     },
 
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
- 
     initialize: function() {
         var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function() {
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
     }
 
